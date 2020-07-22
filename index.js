a9959c791d9938186b558f5d0e544e425e21c1e8
ReginaF2012 <skritschy@gmail.com>
2020-07-21 19:59:45 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObj) {
+  const results = arrayOfObj.find( function (obj){
+    return obj.result === "W"
+  });
+  return results === undefined ? undefined : results.year
+};
