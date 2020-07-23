2c4d00f5f7b65fd1289092623018ba511f616d58
Bryce-Du <bryce.dumas@gmail.com>
2020-07-23 06:47:07 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const win = record.find(e => e.result === "W")
+  return win !== undefined ? win.year : win
+}
