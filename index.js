7c4531261043f3000f700575bc72fd7a428fbbd6
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-26 19:58:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+// const record = [
+//   { year: "2018", result: "L" },
+//   { year: "2017", result: "W" },
+//   { year: "2016", result: "N/A" }
+// ]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+    let found = array.find(function(array) { 
+      return array.result === "W"})
+    if (!!found) { 
+      return found.year
+    }
+    return undefined
 }
+
+// console.log(superbowlWin(record))
