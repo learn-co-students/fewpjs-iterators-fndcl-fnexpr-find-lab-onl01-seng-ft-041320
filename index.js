45f13bbdf52f86e4618fdb3206414527f46d9479
codeweb123 <jiyonj@hotmail.com>
2020-07-29 16:23:51 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arr) => {
+  const win = arr.find(data=> data.result === 'W')
+  if (win) {
+    return win.year
+  }
+}
