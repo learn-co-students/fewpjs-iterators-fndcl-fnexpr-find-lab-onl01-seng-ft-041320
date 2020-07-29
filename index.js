ee8e82fcee162256401f625369b6f144b1833a5e
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-29 12:54:42 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(bowls) {
+
+  let win = bowls.find( (bowl) => {
+    return bowl.result === "W" 
+  });
+
+  if (win === undefined) {
+    return undefined
+  } else {
+    return win.year
+  }
+}
